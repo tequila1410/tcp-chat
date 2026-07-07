54e4a9ed80e80587cd6945e43a7cb4492b1c46fb
Vlado <vprokhorov.work@gmail.com>
2026-07-07 20:37:23 +0300

    refactoring server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -27,9 +27,13 @@ fn main() -> io::Result<()> {
         };
         {
             let mut clients_lock = clients_clone.lock().unwrap();
-            clients_lock.push((peer_addr, stream.try_clone().unwrap()));
+            match stream.try_clone() {
+                Ok(val) => clients_lock.push((peer_addr, val)),
+                Err(error) => eprintln!("Failed to clone stream for client {peer_addr}: {error}")
+            }
         }
         thread::spawn(move || {
+            println!("Client connected: {peer_addr}");
             if let Err(error) = handle_client(stream, peer_addr, clients_clone) {
                 eprintln!("Client error {peer_addr}: {error}");
             }
@@ -45,39 +49,39 @@ fn handle_client(mut stream: TcpStream, peer_addr: SocketAddr, clients: Clients)
         let bytes_read = match stream.read(&mut buffer) {
             Ok(0) => {
                 println!("Client disconnected: {peer_addr}");
-                {
-                    let mut clients = clients.lock().unwrap();
-                    clients.retain(|(addr, _)| *addr != peer_addr);
-                }
+                remove_client(&clients, peer_addr);
                 return Ok(());
             }
             Ok(n) => n,
             Err(error) if is_client_disconnect_error(&error) => {
                 println!("Client reset connection: {peer_addr}");
-                {
-                    let mut clients = clients.lock().unwrap();
-                    clients.retain(|(addr, _)| *addr != peer_addr);
-                }
+                remove_client(&clients, peer_addr);
                 return Ok(());
             }
             Err(error) => return Err(error),
         };
 
-        let mut clients = clients.lock().unwrap();
-        
-        println!("{:#?}", clients);
-        for (client_addr, client_stream) in clients.iter_mut() {
-            if *client_addr == peer_addr {
-                continue;
-            }
-            match client_stream.write_all(&buffer[..bytes_read]) {
-                Ok(()) => {},
-                Err(error) if is_client_disconnect_error(&error) => {
-                    println!("Client reset connection: {peer_addr}");
+        let mut disconnected_clients = Vec::new();
+        {
+            let mut clients = clients.lock().unwrap();
+            
+            for (client_addr, client_stream) in clients.iter_mut() {
+                if *client_addr == peer_addr {
                     continue;
                 }
-                Err(error) => return Err(error),
+                match client_stream.write_all(&buffer[..bytes_read]) {
+                    Ok(()) => {},
+                    Err(error) if is_client_disconnect_error(&error) => {
+                        disconnected_clients.push(*client_addr);
+                        continue;
+                    }
+                    Err(error) => return Err(error),
+                }
             }
+        }
+
+        for client_addr in disconnected_clients {
+            remove_client(&clients, client_addr);
         }
         
     }
@@ -90,6 +94,7 @@ fn is_client_disconnect_error(error: &io::Error) -> bool {
     )
 }
 
-fn remove_client() {
-    
+fn remove_client(clients: &Clients, peer_addr: SocketAddr) {
+    let mut clients_lock = clients.lock().unwrap();
+    clients_lock.retain(|(addr, _)| *addr != peer_addr);
 }
