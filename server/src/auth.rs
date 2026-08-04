5ca775152418c45dd2b5305d88d7e90df81b35ba
Vladyslav Zubko <vlad.zubko.sk@gmail.com>
2026-08-04 15:13:08 +0200

    refactor auth.rs and add tests

--- server/src/auth.rs
+++ server/src/auth.rs
@@ -8,12 +8,16 @@ use crate::client::{ClientRegistry, SessionId};
 pub type Credentials = Arc<HashMap<String, String>>;
 
 pub async fn authenticate(client_registry: &ClientRegistry, session_id: SessionId, credentials: &Credentials, login: String, password: String) {
-    if let Some(db_pass) = credentials.get(&login) && *db_pass == password {
+    if is_valid_credentials(credentials, &login, &password) {
         client_registry.authorize_client(session_id, login).await;
-        client_registry.reply(session_id,  ServerMessage::AuthOk).await;
+        client_registry.reply(session_id, ServerMessage::AuthOk).await;
     } else {
-        client_registry.reply(session_id,  ServerMessage::AuthErr("Not authenticated\n".to_string())).await;
-    };
+        client_registry.reply(session_id,  ServerMessage::AuthErr("Invalid credentials".to_string())).await;
+    }
+}
+
+fn is_valid_credentials(credentials: &Credentials, login: &str, password: &str) -> bool {
+    credentials.get(login).is_some_and(|db_pass| db_pass == password)
 }
 
 pub fn init_credentials() -> Credentials {
@@ -21,4 +25,28 @@ pub fn init_credentials() -> Credentials {
     db.insert("Oliver".to_string(), "123123".to_string());
     db.insert("Emma".to_string(), "123123".to_string());
     Arc::new(db)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_credentials() -> Credentials {
+        let mut db = HashMap::new();
+        db.insert("Alice".to_string(), "secret".to_string());
+        Arc::new(db)
+    }
+
+    #[test]
+    fn test_is_valid_credentials() {
+        let credentials = test_credentials();
+        assert!(is_valid_credentials(&credentials, "Alice", "secret"));
+    }
+
+    #[test]
+    fn test_is_not_valid_credentials() {
+        let credentials = test_credentials();
+        assert!(!is_valid_credentials(&credentials, "Alice", "secret1"));
+        assert!(!is_valid_credentials(&credentials, "Ali", "secret"));
+    }
 }
